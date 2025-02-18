952febf9cc12a44b02ab02fd38debf34850b9ccf
Sebasldu <arielsebastian712@gmail.com>
2025-02-18 18:03:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("formularioContacto").addEventListener("submit", function (event) {
+        event.preventDefault();
+        alert("Noticia ingresada con éxito");
+    });
+});
